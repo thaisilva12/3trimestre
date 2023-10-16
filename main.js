986993bd9046ab57860f51b78b729328d68f68eb
thaisilva12 <133890929+thaisilva12@users.noreply.github.com>
2023-10-16 12:23:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,8 +8,16 @@ let contador = 0;
 
 // Estrutura de repetiçao - Enquanto
 while(contador < listaDeTeclas.length) {
- listaDeTeclas[contador].onclick = function(){
+listaDeTeclas[contador].onclick = function(){
  tocaSom('som_tecla_splash');
  };
 contador = contador + 1;
+}
+const teclas = listaDeTeclas[contador];
+const instrumento = tecla.classList[1];
+const idAudio = '#som_${instrumento}';
+console.log(idAudio);
+console.log(instrumento);
+tecla.onclick = function (){
+tocaSom(idAudio);
 }
