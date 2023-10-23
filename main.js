95e6db9f714c874323c205779fdb7c42719aa8bb
thaisilva12 <133890929+thaisilva12@users.noreply.github.com>
2023-10-23 12:24:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 function tocaSom(idElementoAudio){
      document.querySelector(idElementoAudio).play();
 }
- 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-let contador = 0;
 
 // Estrutura de repetiçao - Enquanto
-while(contador < listaDeTeclas.length) {
-listaDeTeclas[contador].onclick = function(){
- tocaSom('som_tecla_splash');
- };
-contador = contador + 1;
-}
+for(let contador = 0;contador < listaDeTeclas.length;contador++) {
 const teclas = listaDeTeclas[contador];
 const instrumento = tecla.classList[1];
-const idAudio = '#som_${instrumento}';
-console.log(idAudio);
-console.log(instrumento);
+const idAudio = `#som_${instrumento}`;
+}
 tecla.onclick = function (){
 tocaSom(idAudio);
+};
+tecla.onkeydown = function (){
+tecla.classList.add('ativa');
 }
+tecla.onkeyup = function(){
+tecla.classList.remove('ativa');
+} 
+console.log(contador);{
+
+}
+
